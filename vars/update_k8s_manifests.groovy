624630f6164288c63860ac645720716b6c7aec74
Hanzala Israr <hanzala.coder@gmail.com>
2026-05-20 20:33:37 +0500

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -8,7 +8,7 @@ def call(Map config = [:]) {
     def manifestsPath = config.manifestsPath ?: 'kubernetes'
     def gitCredentials = config.gitCredentials ?: 'github-credentials'
     def gitUserName = config.gitUserName ?: 'Hanzala-Israr'
-    def gitUserEmail = config.gitUserEmail ?: 'jenkins@example.com'
+    def gitUserEmail = config.gitUserEmail ?: 'hanzala.coder@gmail.com'
     
     echo "Updating Kubernetes manifests with image tag: ${imageTag}"
     
