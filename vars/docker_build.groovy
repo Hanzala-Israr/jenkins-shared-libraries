0f1aa3e000079a6105eaa723390fce3a8fa56fa4
Hanzala Israr <hanzala.coder@gmail.com>
2026-05-20 22:15:18 +0500

    Update Docker build script to use default image tag

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,11 +1,12 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("imageName parameter is required")
-    def imageTag = config.imageTag ?: error("imageTag parameter is required")
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
     def dockerfile = config.dockerfile ?: 'Dockerfile'
     def context = config.context ?: '.'
-
+    
     echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
     
-    // FIX: Added --security-opt seccomp=unconfined to bypass Ubuntu 24.04 kernel blocks
-    sh "docker build --security-opt seccomp=unconfined -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}"
+    sh """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
 }
