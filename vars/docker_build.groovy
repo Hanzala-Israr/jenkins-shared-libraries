2279e8a7385606055c41601374a1afe484914780
Hanzala Israr <hanzala.coder@gmail.com>
2026-05-20 22:08:26 +0500

    Enhance Docker build script with parameter checks
    Updated error messages for missing parameters and added security option for Docker build.

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,11 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
+    def imageName = config.imageName ?: error("imageName parameter is required")
+    def imageTag = config.imageTag ?: error("imageTag parameter is required")
     def dockerfile = config.dockerfile ?: 'Dockerfile'
     def context = config.context ?: '.'
-    
+
     echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
     
-    sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
-    """
+    // FIX: Added --security-opt seccomp=unconfined to bypass Ubuntu 24.04 kernel blocks
+    sh "docker build --security-opt seccomp=unconfined -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}"
 }
